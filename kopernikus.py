77790a8f666957b17886aa67299c0c495997775f
Car_coder <84738042+car-coder-907@users.noreply.github.com>
2023-07-21 13:36:08 +0200

    Update kopernikus.py

--- kopernikus.py
+++ kopernikus.py
@@ -44,6 +44,7 @@ def compare_frames_change_detection(prev_frame, next_frame, min_contour_area):
     thresh = cv2.threshold(frame_delta, 45, 255, cv2.THRESH_BINARY)[1]
 
     thresh = cv2.dilate(thresh, None, iterations=2)
+    
     cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL,
                             cv2.CHAIN_APPROX_SIMPLE)
     cnts = imutils.grab_contours(cnts)
@@ -76,53 +77,77 @@ def is_valid_image(img):
 
 
 def find_and_remove_similar_images(input_folder):
-    min_contour_area_threshold = 3000 
+    min_contour_area_threshold = 3000
     min_score_threshold = 100000
-    max_size_difference = 0  
+    min_count_threshold = 25000
+    max_size_difference = 0
 
     image_files = [file for file in os.listdir(input_folder) if file.endswith('.png')]
     print(image_files)
     num_images = len(image_files)
 
+    images_to_remove = []
 
     for i in range(num_images):
         img_path_i = os.path.join(input_folder, image_files[i])
         img_i = cv2.imread(img_path_i)
-        
+
         if not is_valid_image(img_i):
             print(f"Warning: Image {img_path_i} is invalid or corrupted. Skipping.")
+            images_to_remove.append(image_files[i])
             continue
-        
+
         img_i = preprocess_image_change_detection(img_i)
 
         for j in range(i + 1, num_images):
             img_path_j = os.path.join(input_folder, image_files[j])
             img_prefix_i = image_files[i][:3]
             img_prefix_j = image_files[j][:3]
-            
+
             # Check if the first 3 letters of the image file names match
             if img_prefix_i != img_prefix_j:
-                print('Images are from two differnt Camera IDs. Skipping')
+                print('Images are from two different Camera IDs. Skipping')
                 continue
-            
+
             img_j = cv2.imread(img_path_j)
             print(f"Comparing Image:{image_files[i]} with Image:{image_files[j]}")
 
             if not is_valid_image(img_j):
                 print(f"Warning: Image {img_path_j} is invalid or corrupted. Skipping.")
+                images_to_remove.append(image_files[j])
                 continue
 
             img_j = preprocess_image_change_detection(img_j)
-        
+
             if are_images_similar_size(img_i, img_j, max_size_difference):
-                
-                score, _, _ = compare_frames_change_detection(img_i, img_j, min_contour_area_threshold)
-        
-                if score < min_score_threshold:
-                    # Images are similar, remove image_files[j] from the input folder and the image_files list
-                    print(f"Images {image_files[i]} and {image_files[j]} are similar. Removing {image_files[j]}.")
-                    os.remove(img_path_j)
-                   
+                score, _, thresh = compare_frames_change_detection(img_i, img_j, min_contour_area_threshold)
+
+                count = 0
+                height, width = thresh.shape
+                for y in range(height):
+                    for x in range(width):
+                        # Check pixel value at (x, y)
+                        pixel_value = thresh[y, x]
+
+                        # Check if the pixel has changed (difference in intensity)
+                        if pixel_value == 255:
+                            count = count + 1
+                print('Count: ', count)
+
+                if score < min_score_threshold and count < min_count_threshold:
+                    # Images are similar, add image_files[j] to the images_to_remove list
+                    images_to_remove.append(image_files[j])
+
+    # Remove images that need to be removed from the original image_files list
+    #image_files = [file for file in image_files if file not in images_to_remove]
+
+    # Perform the removal of similar images from the dataset folder
+    for image_to_remove in images_to_remove:
+        img_path_to_remove = os.path.join(input_folder, image_to_remove)
+        print(f"Removing similar image: {img_path_to_remove}")
+        os.remove(img_path_to_remove)
+
+
 if __name__ == "__main__":
-    input_dataset_folder = r"C:\Users\sb\Downloads\dataset-candidates-ml_final\dataset"
-    find_and_remove_similar_images(input_dataset_folder)
+    input_folder = r"C:\Users\sb\Downloads\dataset-candidates-ml_final\dataset"
+    find_and_remove_similar_images(input_folder)
